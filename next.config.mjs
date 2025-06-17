f7eae802717e2813f9380d25c88a5094cfebb804
muhammed polat <mhmmdpolatt@gmail.com>
2025-06-17 13:57:56 +0300

    change nextjs.ncofig

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: ['mern-stack-server-czfb.onrender.com'],
+        domains: ['mern-stack-server-czfb.onrender.com'],
     },
 };
 
